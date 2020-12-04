a107abdbec33d81a515e18eddc35092464f186e9
yuanshangchi <hyuan@oberlin.edu>
2020-12-04 21:41:41 +0800

    'input file not exists' and truncate result1,2.txt

--- plagiarism.c
+++ plagiarism.c
@@ -2,6 +2,7 @@
 #include "cleanup.h"
 #include "tfidf.h"
 #include "cossim.h"
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) //the thing I think should NOT be perserved in this c file. It should be showed in plagiarlism.c
 {
@@ -10,9 +11,13 @@ int main(int argc, char *argv[]) //the thing I think should NOT be perserved in 
     char *names[100] = {"result1.txt", "result2.txt"};
     for (int i = 1; i < argc; i++)
     {
-        
         fp = fopen(argv[i], "r");
-        newfp = fopen(names[index], "a");
+        if (fp == NULL)
+        {
+            printf("File not exists: %s\n", argv[i]);
+            exit(1);
+        }
+        newfp = fopen(names[index], "w");
         cleanup(fp, newfp);
         fclose(fp);
         fclose(newfp);
